fec668acd98bf581cbc505e51752d707fd4cdbb7
dzachara <dzachara@ZACHARA-D>
2021-12-16 11:45:17 +0100

    Add project files.

---
+++ L2_4_Zad_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace L2_4_Zad_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte numberOne;
+            float numberTwo;
+            string name;
+
+            numberOne = 10;
+            name = "Szkoła Dotneta";
+            numberTwo = 12.5f;
+        }
+    }
+}
